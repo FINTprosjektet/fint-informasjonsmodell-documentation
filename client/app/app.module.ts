10d6d6d9903c0985aacb75b921359923b472a4bc
Oystein Amundsen <oystein.amundsen@bouvet.no>
2017-02-03 16:37:09 +0100

    Removed annoying dependency bug

--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule, Request, XSRFStrategy } from '@angular/http';
+import { HttpModule, Request } from '@angular/http';
 import { MaterialModule } from '@angular/material';
 
 import { LibSharedModule } from 'fint-shared-components';
@@ -27,7 +27,7 @@ import { ModelComponent } from './views/model/model.component';
     AppRoutingModule,
     ResultModule
   ],
-  providers: [ModelService], // !!HACK!!
+  providers: [ModelService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
